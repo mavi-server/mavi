b0cdad13b521d67943d487de5481caa4a63ceee2
m.emre yalçın <99.emreyalcin@gmail.com>
2022-01-31 02:13:13 +0300

    patch

--- cli/bin/blue-server.js
+++ cli/bin/blue-server.js
@@ -25,10 +25,15 @@ switch (command) {
     script = path.join(__dirname, `../src/commands/remove-models.js`)
     require(script)
     break;
-  default:
   case "start":
+  default:
+    console.log('\x1b[36mApplying Models...\x1b[0m')
+    script = path.join(__dirname, `../src/commands/apply-models.js`)
+    require(script)
+
     console.log('\x1b[36mStarting Blue Server...\x1b[0m')
     script = path.join(__dirname, `../../dist/index.js`)
+
     require(script).createServer(config)
     break;
 }
