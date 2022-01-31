744ce9b825e62a55d112af273b793cd1c6e28b00
m.emre yalçın <99.emreyalcin@gmail.com>
2022-01-31 19:19:37 +0300

    last one

--- cli/bin/blue-server.js
+++ cli/bin/blue-server.js
@@ -6,17 +6,19 @@ const command = process.argv[2]
 const { version } = require(path.join(__dirname, '../../package.json'))
 let script
 
-// neccessary for finding some relative paths in the config file
-config.__dirname = path.join(__dirname, '../../config')
-
 if (!config) {
   console.log('No config file found. Please create a config file at ./index.js')
   process.exit(1)
 }
 
+const mode = process.env.NODE_ENV || 'development'
+
+// neccessary for finding some relative paths in the config file
+config.__dirname = path.join(__dirname, '../../config')
+
 switch (command) {
   case "dev":
-    console.log(`\x1b[36mStarting Sub Module Blue Server ${version} ${process.env.NODE_ENV}...\x1b[0m`)
+    console.log(`\x1b[36mStarting Sub-module Blue Server ${version} ${mode}...\x1b[0m`)
     script = path.join(__dirname, `../../index.js`)
     require(script).createServer(config)
   case "apply":
@@ -31,7 +33,7 @@ switch (command) {
     break;
   case "start":
   default:
-    console.log(`\x1b[36mStarting Blue Server ${version} ${process.env.NODE_ENV}...\x1b[0m`)
+    console.log(`\x1b[36mStarting Blue Server ${version} ${mode}...\x1b[0m`)
 
     console.log(`\x1b[36mApplying Models...\x1b[0m`)
     script = path.join(__dirname, `../src/commands/apply-models.js`)
