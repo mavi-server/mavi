a5fe30197313613d7e7d061f3f88726fa2d254af
m.emre yalçın <99.emreyalcin@gmail.com>
2022-03-13 18:11:45 +0300

    refactor: database config file

--- database/index.js
+++ database/index.js
@@ -1,22 +1,27 @@
 // Knex Documentation: https://knexjs.org/
-const mode = (process.env.NODE_ENV || 'development').toLowerCase()
+const knex = require('knex')
+const mode = process.env.NODE_ENV || 'development'
 
 const createDatabase = (database) => {
-  const cfg = mode === 'production' ? database.production : database.development
-  const knex = require('knex')(cfg)
+  if (mode in database) {
+    const config = database[mode]
 
-  // if (cfg.debug === true) {
-  //   let counter = 0
-  //   knex.on('query', function (data) {
-  //     counter++
-  //     console.log(`[${counter}. sql]`, `\x1b[35m${data.sql}\x1b[0m`, data.bindings)
-  //   })
-  //   knex.on('query-error', function (err) {
-  //     throw Error(err.message)
-  //   })
-  // }
+    // if (config.debug === true) {
+    //   let counter = 0
+    //   knex.on('query', function (data) {
+    //     counter++
+    //     console.log(`[${counter}. sql]`, `\x1b[35m${data.sql}\x1b[0m`, data.bindings)
+    //   })
+    //   knex.on('query-error', function (err) {
+    //     throw Error(err.message)
+    //   })
+    // }
 
-  return knex
+    return knex(config)
+  }
+  else {
+    throw new Error(`Database mode \x1b[31m${mode}\x1b[0m not found`)
+  }
 }
 
 module.exports = createDatabase
