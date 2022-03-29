9bb5362e43852d8196fbe024385432fe52c5464f
m.emre yalçın <99.emreyalcin@gmail.com>
2022-03-29 22:46:52 +0300

    fix(cli): improved & `-apply, -no-seed, -v` added

--- cli/src/commands/remove-models.js
+++ cli/src/commands/remove-models.js
@@ -1,16 +1,26 @@
 const path = require('path');
-const modelsTable = 'DATABASE_STATE';
+const DB_STATE_TABLE = 'DATABASE_STATE';
 const configPath = path.join(process.cwd(), './index.js');
 const config = require(configPath);
-
-// Database connection
 const knex = require(path.join(__dirname, '../../../database'))(config.database);
 
-// remove modelsTable:
-const DB_STATE = knex.schema.hasTable(modelsTable);
-if (DB_STATE) {
-  knex.schema.dropTable(modelsTable).then(() => {
-    console.log('\x1b[34mModels removed!\x1b[0m');
-    process.exit(0);
+module.exports = async () => {
+  // drop db state:
+  await knex.schema.hasTable(DB_STATE_TABLE).then(exists => {
+    if (exists) {
+      return knex.schema.dropTable(DB_STATE_TABLE);
+    }
   });
-}
+
+  // drop models:
+  for (const model in config.api.define.models) {
+    await knex.raw(`DROP TABLE IF EXISTS ${model} CASCADE`)
+      .then(() => {
+        console.log(`\x1b[36mRemoved table "${model}"\x1b[0m`);
+      })
+      .catch(err => {
+        console.log(`\x1b[31mError removing table "${model}"\x1b[0m`);
+        console.log(err);
+      });
+  }  
+};
--- cli/src/commands/reset-schema.js
+++ cli/src/commands/reset-schema.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const schema = 'public';
+const SCHEMA = 'public';
 const configPath = path.join(process.cwd(), './index.js');
 const config = require(configPath);
 
@@ -7,10 +7,9 @@ const config = require(configPath);
 const knex = require(path.join(__dirname, '../../../database'))(config.database);
 
 // drop schema
-knex.schema.dropSchemaIfExists(schema, true).then(async () => {
+module.exports = () => {
+  return knex.schema.dropSchemaIfExists(SCHEMA, true).then(async () => {
   // create schema
-  await knex.raw(`CREATE SCHEMA ${schema}`);
-
-  // exit process
-  process.exit(0);
-});
+    await knex.raw(`CREATE SCHEMA ${SCHEMA}`);
+  });
+};
