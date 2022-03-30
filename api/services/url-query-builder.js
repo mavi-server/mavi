e2f36c3de99fa4f5d17690a069cec81c9f56eb8b
m.emre yalçın <99.emreyalcin@gmail.com>
2022-03-30 23:17:38 +0300

    refactor: url-query-builder

--- api/services/url-query-builder.js
+++ api/services/url-query-builder.js
@@ -1,4 +1,7 @@
-const UrlQueryBuilder = (query, columns) => {
+/** @type {(query: any, columns: array, payload: { row: any, context: string, params: object }) => any} */
+const UrlQueryBuilder = (Query, columns, { context, row, params }) => {
+  let query = { ...Query };
+
   // Builder:
   for (const key in query) {
     switch (key) {
@@ -31,62 +34,8 @@ const UrlQueryBuilder = (query, columns) => {
         // examples:
         // simple where: where=id-1-and-community-eq-1-or-name-like-%test%
         if (typeof query[key] === 'string') {
-          const createParameters = group => {
-            if (!group || !group.part) return;
-
-            let [column, condition, value] = group.part.split('-');
-            let operator = '='; // default
-            column = column || 'id';
-            condition = condition || 'eq';
-
-            // if condition is not specified, value would be false
-            if (typeof value === 'undefined') {
-              value = condition; // condition become actual value
-              condition = 'eq'; // default condition is 'equal'
-            }
-
-            // condition and value is not specified
-            if (condition == value) {
-              value = null;
-            }
-
-            // value is null
-            if (value === 'null') value = null;
-            else if (value === 'undefined') value = null;
-
-            // convert the condition into operator so that knex can understand it
-            switch (condition) {
-              case 'like':
-                operator = 'like';
-                break;
-              case 'not':
-              case 'neq':
-                operator = '<>';
-                break;
-              case 'eq':
-                operator = '=';
-                break;
-              case 'lg':
-                operator = '>';
-                break;
-              case 'lge':
-                operator = '>=';
-                break;
-              case 'sm':
-                operator = '<';
-                break;
-              case 'sme':
-                operator = '<=';
-                break;
-              default:
-                operator = '=';
-                break;
-            }
-
-            return [column, operator, value];
-          };
           // split the query into groups
-          const reg = /-and-|-or-/g;
+          const reg = /-and-|-or-|\sand\s|\sor\s/g;
           const groupCount = query[key].split(reg).length;
           const Groups = new Array(groupCount).fill({});
 
@@ -135,8 +84,15 @@ const UrlQueryBuilder = (query, columns) => {
             Groups[0].params = createParameters(Groups[0]);
           }
 
-          query[key] = Groups;
-          // console.log(query[key])
+          if ((context && row) || params) {
+            query[key] = placeSpecialParameters(Groups, {
+              context,
+              row,
+              params,
+            });
+          } else {
+            query[key] = Groups;
+          }
         }
         break;
       case 'exclude':
@@ -154,6 +110,119 @@ const UrlQueryBuilder = (query, columns) => {
         break;
     }
   }
+
+  return query;
+};
+const createParameters = group => {
+  if (!group || !group.part) return;
+
+  let [column, condition, value] = group.part.split('-');
+  let operator = '='; // default
+  column = column || 'id';
+  condition = condition || 'eq';
+
+  // if condition is not specified, value would be false
+  if (typeof value === 'undefined') {
+    value = condition; // condition become actual value
+    condition = 'eq'; // default condition is 'equal'
+  }
+
+  // condition and value is not specified
+  if (condition == value) {
+    value = null;
+  }
+
+  // value is null
+  if (value === 'null') value = null;
+  else if (value === 'undefined') value = null;
+
+  // convert the condition into operator so that knex can understand it
+  switch (condition) {
+    case 'like':
+      operator = 'like';
+      break;
+    case 'not':
+    case 'neq':
+      operator = '<>';
+      break;
+    case 'eq':
+      operator = '=';
+      break;
+    case 'lg':
+      operator = '>';
+      break;
+    case 'lge':
+      operator = '>=';
+      break;
+    case 'sm':
+      operator = '<';
+      break;
+    case 'sme':
+      operator = '<=';
+      break;
+    default:
+      operator = '=';
+      break;
+  }
+
+  return [column, operator, value];
+};
+const placeSpecialParameters = (group, { context, row, params }) => {
+  // assign special variables to the query
+  if (group)
+    for (const w of group) {
+      w.params
+        .filter(p => typeof p === 'string')
+        .filter(p => p.includes('row.') || p === '#context')
+        .forEach(p => {
+          let key;
+
+          if (p === '#context') {
+            // replace `#context` with the current context
+            // context is the parent model name
+            w.params[2] = context;
+
+            // in some cases the `context` can be defined as a special string
+            // eg. parentPopulateObject: { from: row.key, populate: ['x'] }
+            // x's `context` will be 'row.key'
+            if (context.startsWith('row.')) {
+              key = context.split('row.')[1];
+            }
+          } else if (p.startsWith('row.')) {
+            // you can assign row values to the where parameters
+            // e.g. query: { where: 'id-eq-row.id' } (inside of the populate object)
+            key = p.split('row.')[1]; // get the key from the parameter
+          } else if (p.startsWith('req.params.')) {
+            // you can assign req.params values to the where parameters
+            // if params are defined corresponding values will be used
+            // get variable string
+            key = p.split('req.params.')[1];
+          }
+
+          if (row) {
+            // throw error if row column is undefined
+            if (typeof key !== 'undefined' && !(key in row)) {
+              throw Error(
+                `sub-controller: row.${key} is not defined for params: "${w.params}"`
+              );
+            }
+
+            // assign the row value to the where parameter
+            p = row[key];
+          } else if (params) {
+            // don't allow to use if key is not in the req.params
+            if (!(key in params)) {
+              throw new Error('Missing parameter: ' + key);
+            }
+
+            // assign the req.params value to the where parameter
+            p = params[key];
+          }
+        });
+    }
+  else group = [];
+
+  return group;
 };
 
 module.exports = UrlQueryBuilder;
