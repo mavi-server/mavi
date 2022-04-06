78c59095a5fa31398bf5153b82322fcf69d8ce7d
m.emre yalçın <99.emreyalcin@gmail.com>
2022-04-06 18:48:51 +0300

    fix(cli): create trigger functions before apply

--- cli/src/commands/reset-schema.js
+++ cli/src/commands/reset-schema.js
@@ -9,7 +9,7 @@ const knex = require(path.join(__dirname, '../../../database'))(config.database)
 // drop schema
 module.exports = () => {
   return knex.schema.dropSchemaIfExists(SCHEMA, true).then(async () => {
-  // create schema
+    // create schema
     await knex.raw(`CREATE SCHEMA ${SCHEMA}`);
   });
 };
